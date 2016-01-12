672953e5036565dab36ea6c9747664c584ff78c3
Judd Cohen <jcohen@juddnet.com>
2016-01-12 12:15:18 -0800

    Made windows build script use relative paths

--- make.cmd
+++ make.cmd
@@ -3,7 +3,7 @@
 SET PYTHONPATH=%PYTHONPATH%:.\pypy-source
 
 REM Make with -O0:
-REM C:\pypy\pypy.exe pypy-source\rpython\bin\rpython -O0 --gc=hybrid dipper.py
+REM pypy pypy-source\rpython\bin\rpython -O0 --gc=hybrid dipper.py
 
 REM Make with -O2:
-C:\pypy\pypy.exe pypy-source\rpython\bin\rpython dipper.py
+pypy pypy-source\rpython\bin\rpython dipper.py
